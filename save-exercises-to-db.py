af2c049bd201e52e29e2525b283850cd7c89a155
Magnus Lundstrom <magnuslundstromdk@gmail.com>
2022-10-22 21:50:17 +0200

    write insert exercises to db script

--- save-exercises-to-db.py
+++ save-exercises-to-db.py
@@ -1,17 +1,47 @@
-import mysql.connector
-import json
 from utils import get_exercises, get_db
+import json
+
 
 def save_to_db():
-	db = get_db()
-	cursor = db.cursor()
+    db = get_db()
+    cursor = db.cursor()
 
-	exercises = map(lambda a: [a], get_exercises(1))
-	sql = "INSERT INTO exercises (name) VALUES (%s)"
+    # exercises = map(
+    #     lambda a: [
+    #         a["title"],
+    #         a["image"] if "image" in a else None,
+    #         a["rating"],
+    #         a["muscle_group"],
+    #     ],
+    #     get_exercises(),
+    # )
 
-	print(exercises)
+	
+    all_exercises = get_exercises()
+    exercises = []
 
+    for exercise in all_exercises:
+        muscleGroup = exercise["muscle_group"].replace(" ", "_")
+        exercises.append([
+			exercise["title"],
+			exercise["image"] if "image" in exercise else None,
+			exercise["rating"],
+			muscleGroup,
+		])
 
+	
+	
+
+    sql = "INSERT INTO exercises (name, image, rating, muscleGroup) VALUES (%s, %s, %s, %s)"
+
+    cursor.executemany(sql, exercises)
+
+    db.commit()
+
+    print(cursor.rowcount, "was inserted.")
+
+    cursor.close()
+    db.close()
 
 
 save_to_db()
--- utils.py
+++ utils.py
@@ -1,32 +1,31 @@
 import mysql.connector
 import json
 
-def get_exercises(fixed_amount):
-	exercises_json = open('exercises.json')
-	data = json.load(exercises_json)
-	if fixed_amount:
-		return data[:fixed_amount]
 
-	return data
+def get_exercises():
+    exercises_json = open("exercises.json")
+    data = json.load(exercises_json)
+
+    return data
 
 
 def get_muscle_groups():
-	muscle_groups_json = open('muscle_groups.json')
-	data = json.load(muscle_groups_json)
-	return data
+    muscle_groups_json = open("muscle_groups.json")
+    data = json.load(muscle_groups_json)
+    return data
 
 
 def get_db():
-	try:
-		db = mysql.connector.connect(
-			host="localhost",
-			user="root",
-			password="workout",
-			database="workout",
-			port=3306
-		)
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="workout",
+            database="workout",
+            port=3306,
+        )
 
-		return db
+        return db
 
-	except mysql.connector.Error as err:
-		print(err)		
+    except mysql.connector.Error as err:
+        print(err)
